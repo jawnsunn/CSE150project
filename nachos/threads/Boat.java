9f2b588b04c65583ff8fd9f72227bd746e81c6b8
zongwesleylee <43146698+zongwesleylee@users.noreply.github.com>
2019-10-28 22:46:01 -0700

    Update Boat.java

--- nachos/threads/Boat.java
+++ nachos/threads/Boat.java
@@ -1,71 +1,89 @@
 package nachos.threads;
-import nachos.ag.BoatGrader;
+import java.util.PriorityQueue;
+import nachos.machine.*;
+/**
+ * Uses the hardware timer to provide preemption, and to allow threads to sleep
+ * until a certain time.
+ */
+public class Alarm {
+	/**
+	 * Allocate a new Alarm. Set the machine's timer interrupt handler to this
+	 * alarm's callback.
+	 *
+	 * 
+	 * 
+	 * <p><b>Note</b>: Nachos will not function correctly with more than one
+	 * alarm.
+	 */
 
-public class Boat
-{
-    static BoatGrader bg;
-    
-    public static void selfTest()
-    {
-	BoatGrader b = new BoatGrader();
+	public Alarm() {
+
+		Machine.timer().setInterruptHandler(new Runnable() {
+			public void run() { timerInterrupt(); }
+		});
+	}
+
+	/**
+	 * The timer interrupt handler. This is called by the machine's timer
+	 * periodically (approximately every 500 clock ticks). Causes the current
+	 * thread to yield, forcing a context switch if there is another thread
+	 * that should be run.
+	 */
 	
-	System.out.println("\n ***Testing Boats with only 2 children***");
-	begin(0, 2, b);
+	private PriorityQueue<threadTime> waitQueue = new PriorityQueue<threadTime>();
 
-//	System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
-//  	begin(1, 2, b);
-
-//  	System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
-//  	begin(3, 3, b);
-    }
-
-    public static void begin( int adults, int children, BoatGrader b )
-    {
-	// Store the externally generated autograder in a class
-	// variable to be accessible by children.
-	bg = b;
-
-	// Instantiate global variables here
+	private class threadTime implements Comparable<threadTime>{
+		public KThread thread;
+		public long wakeTime;
+		public threadTime (KThread thread, long wakeTime) {
+			this.thread = thread;
+			this.wakeTime = waketime;
+		}
+		public int compareTo(threadTime thTime) {
+			return Long.signum(wakeTime - thTime.wakeTime);
+			}
+		}
+		private KThread thread;
+		private long waketime;
+	}
 	
-	// Create threads here. See section 3.4 of the Nachos for Java
-	// Walkthrough linked from the projects page.
+	public void timerInterrupt() {
 
-	Runnable r = new Runnable() {
-	    public void run() {
-                SampleItinerary();
-            }
-        };
-        KThread t = new KThread(r);
-        t.setName("Sample Boat Thread");
-        t.fork();
+		Machine.interrupt().disable();
+		long currTime = Machine.timer().getTime();
 
-    }
+		while(waitQueue != null && waitQueue.peek().wakeTime <= currTime){
+			threadTime thTime = waitQueue.poll();
+			KThread thread = thTime.thread;
+			if(thread != null)
+				thread.ready();
+		}
+		
+		Machine.interrupt().enable();
+		KThread.yield();
+	}
 
-    static void AdultItinerary()
-    {
-	/* This is where you should put your solutions. Make calls
-	   to the BoatGrader to show that it is synchronized. For
-	   example:
-	       bg.AdultRowToMolokai();
-	   indicates that an adult has rowed the boat across to Molokai
-	*/
-    }
-
-    static void ChildItinerary()
-    {
-    }
-
-    static void SampleItinerary()
-    {
-	// Please note that this isn't a valid solution (you can't fit
-	// all of them on the boat). Please also note that you may not
-	// have a single thread calculate a solution and then just play
-	// it back at the autograder -- you will be caught.
-	System.out.println("\n ***Everyone piles on the boat and goes to Molokai***");
-	bg.AdultRowToMolokai();
-	bg.ChildRideToMolokai();
-	bg.AdultRideToMolokai();
-	bg.ChildRideToMolokai();
-    }
-    
+	/**
+	 * Put the current thread to sleep for at least <i>x</i> ticks,
+	 * waking it up in the timer interrupt handler. The thread must be
+	 * woken up (placed in the scheduler ready set) during the first timer
+	 * interrupt where
+	 *
+	 * <p><blockquote>
+	 * (current time) >= (WaitUntil called time)+(x)
+	 * </blockquote>
+	 *
+	 * @param	x	the minimum number of clock ticks to wait.
+	 *
+	 * @see	nachos.machine.Timer#getTime()
+	 */
+	public void waitUntil(long x) {
+		
+		// for now, cheat just to get something working (busy waiting is bad)
+		Machine.interrupt().disable();
+		long wakeTime = Machine.timer().getTime() + x;
+		waitQueue.add(new threadTime(KThread.currentThread(), wakeTime));
+		KThread.sleep();
+		Machine.interrupt().enable();
+	}
 }
